83cd7d1d88692002783183cb226364d7975e582c
Tom <thomas.spencer@codurance.com>
2020-08-24 22:54:26 +0100

    Further refactor for totalize

--- dist/shopping_basket.js
+++ dist/shopping_basket.js
@@ -4,6 +4,6 @@ exports.totalize = void 0;
 exports.totalize = function (basket) {
     if (basket.length == 0)
         return 0.0;
-    return basket.reduce(function (total, item) { return total += item.unitPrice * item.quantity; }, 0);
+    return basket.reduce(function (total, item) { return total + (item.unitPrice * item.quantity); }, 0);
 };
 //# sourceMappingURL=shopping_basket.js.map
--- shopping_basket.ts
+++ shopping_basket.ts
@@ -2,5 +2,5 @@ export const totalize = (basket) => {
   if (basket.length == 0)
     return 0.0;
 
-  return basket.reduce((total, item) => total += item.unitPrice * item.quantity, 0);
+  return basket.reduce((total, item) => total + (item.unitPrice * item.quantity), 0);
 }
